2085168c25c06f19bb3528049ccc7ba5195a94c6
XiaoWu <violetwx@qq.com>
2014-03-12 15:29:27 +0800

    The first version

---
+++ print/Models/BLL/TradeUtilizer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using print.Models.Common;
+using Top.Api.Request;
+using Top.Api.Response;
+using Top.Api;
+using Top.Api.Domain;
+
+namespace print.Models.BLL
+{
+    public class TradeUtilizer
+    {
+        public static Trade GetTrade(int tid,string sessionKey,out string error)
+        {
+            error = string.Empty;
+            try
+            {
+                ITopClient client = new DefaultTopClient(Constants.ApiUrl, Constants.AppKey, Constants.AppSecret);
+                TradeFullinfoGetRequest req = new TradeFullinfoGetRequest();
+                req.Fields = "buyer_nick,receiver_state,receiver_city,receiver_address,receiver_mobile";
+                req.Tid = tid;
+                TradeFullinfoGetResponse rsp = client.Execute(req, sessionKey);
+                return rsp.Trade;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return new Trade();
+            }
+        }
+
+        public static List<Trade> GetUndeliveredTrades(string sessionKey,out string error)
+        {
+            error = string.Empty;
+            try
+            {
+                ITopClient client = new DefaultTopClient(Constants.ApiUrl, Constants.AppKey, Constants.AppSecret);
+                TradesSoldGetRequest req = new TradesSoldGetRequest();
+                req.Fields = "tid,buyer_nick,receiver_state,receiver_city,receiver_address,receiver_mobile";
+                req.Status = "WAIT_SELLER_SEND_GOODS";
+                req.StartCreated = DateTime.Now.AddMonths(-3);
+                req.EndCreated = DateTime.Now;
+                TradesSoldGetResponse rsp = client.Execute(req, sessionKey);
+                return rsp.Trades;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+                return new List<Trade>();
+            }
+             
+        }
+    }
+}
---
+++ print/Models/Common/Constants.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace print.Models.Common
+{
+    public class Constants
+    {
+        public static bool IsSandBox
+        {
+            get{
+                bool result;
+                if (bool.TryParse(System.Configuration.ConfigurationManager.AppSettings["isSandBox"], out result))
+                    return result;
+                else return false;
+                }
+        }
+
+        public static string ApiUrl
+        {
+            get
+            {
+                if (IsSandBox) { return SandBoxConstants.apiUrl; }
+                else { return ZhengshiConstants.apiUrl; }
+            }
+        }
+
+        public static string AuthUrl
+        {
+            get
+            {
+                if (IsSandBox){return SandBoxConstants.authUrl;}
+                else {return ZhengshiConstants.authUrl; }
+            }
+        }
+
+        public static string AppKey
+        {
+            get
+            {
+                if (IsSandBox) { return SandBoxConstants.appkey; }
+                else { return ZhengshiConstants.appkey; }
+            }
+        }
+
+        public static string AppSecret
+        {
+            get
+            {
+                if (IsSandBox) { return SandBoxConstants.appsecret; }
+                else { return ZhengshiConstants.appsecret; }
+            }
+        }
+    }
+
+    class SandBoxConstants //沙盒参数
+    {
+        public static readonly string apiUrl = "http://gw.api.tbsandbox.com/router/rest";
+        public static readonly string authUrl = String.Format("http://container.api.tbsandbox.com/container?appkey={0}&encode=utf-8", appkey);
+        public static readonly string appkey = "1021749945";
+        public static readonly string appsecret = "sandbox800e89714426f18a255afdae4";
+    }
+
+    class ZhengshiConstants //正式环境参数
+    {
+        public static readonly string apiUrl="http://gw.api.taobao.com/router/rest";
+        public static readonly string authUrl = String.Format("http://container.api.taobao.com/container?appkey={0}&encode=utf-8", appkey);
+        public static readonly string appkey = "21749945";
+        public static readonly string appsecret = "5c66960800e89714426f18a255afdae4";
+    }
+}
