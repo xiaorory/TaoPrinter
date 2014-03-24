91e63880e2447a00c5cf8f6f9d57fcf0ef74cf46
XiaoWu <violetwx@qq.com>
2014-03-24 08:34:22 +0800

    Add other trade status to query

--- print/Models/BLL/TradeUtilizer.cs
+++ print/Models/BLL/TradeUtilizer.cs
@@ -7,14 +7,24 @@ using Top.Api.Request;
 using Top.Api.Response;
 using Top.Api;
 using Top.Api.Domain;
+using print.Models.DAL;
 
 namespace print.Models.BLL
 {
     public class TradeUtilizer
     {
-        public static Trade GetTrade(int tid,string sessionKey,out string error)
+        /// <summary>
+        /// 获取某个订单的详情
+        /// </summary>
+        /// <param name="tid">trade id</param>
+        /// <param name="sessionKey"></param>
+        /// <param name="error">如果出错，则储存错误信息，否则为空。</param>
+        /// <returns>如果操作成功，则返回true。否则为false。</returns>
+        public static bool GetTrade(int tid,string sessionKey,out string error,out Trade trade)
         {
+            bool isSuccess = false;
             error = string.Empty;
+            trade = null;
             try
             {
                 ITopClient client = new DefaultTopClient(Constants.ApiUrl, Constants.AppKey, Constants.AppSecret);
@@ -22,35 +32,108 @@ namespace print.Models.BLL
                 req.Fields = "buyer_nick,receiver_state,receiver_city,receiver_address,receiver_mobile";
                 req.Tid = tid;
                 TradeFullinfoGetResponse rsp = client.Execute(req, sessionKey);
-                return rsp.Trade;
+                trade = rsp.Trade;
+                isSuccess = true;
             }
             catch (Exception ex)
             {
                 error = ex.Message;
-                return new Trade();
             }
+            return isSuccess;
         }
 
-        public static List<Trade> GetUndeliveredTrades(string sessionKey,out string error)
+        /// <summary>
+        /// 获取未发货的订单
+        /// </summary>
+        /// <param name="sessionKey"></param>
+        /// <param name="error">如果出错，则储存错误信息，否则为空。</param>
+        /// <param name="trades">如果成功，返回未发货订单的list。否则为null</param>
+        /// <returns>如果操作成功，则返回true。否则为false。</returns>
+        public static bool GetUndeliveredTrades(string sessionKey,out string error,out List<Trade> trades)
         {
+            bool isSuccess = false;
             error = string.Empty;
+            trades = null;
             try
             {
                 ITopClient client = new DefaultTopClient(Constants.ApiUrl, Constants.AppKey, Constants.AppSecret);
                 TradesSoldGetRequest req = new TradesSoldGetRequest();
                 req.Fields = "tid,buyer_nick,receiver_state,receiver_city,receiver_address,receiver_mobile";
-                req.Status = "WAIT_SELLER_SEND_GOODS";
+                req.Status = TradeStatus.WAIT_SELLER_SEND_GOODS;
                 req.StartCreated = DateTime.Now.AddMonths(-3);
                 req.EndCreated = DateTime.Now;
                 TradesSoldGetResponse rsp = client.Execute(req, sessionKey);
-                return rsp.Trades;
+                trades = rsp.Trades;
+                isSuccess = true;
             }
             catch (Exception ex)
             {
                 error = ex.Message;
-                return new List<Trade>();
             }
-             
+            return isSuccess;
         }
+
+        /// <summary>
+        /// 获取部分发货的订单
+        /// </summary>
+        /// <param name="sessionKey"></param>
+        /// <param name="error">如果出错，则储存错误信息，否则为空。</param>
+        /// <param name="trades">如果成功，返回未发货订单的list。否则为null</param>
+        /// <returns>如果操作成功，则返回true。否则为false。</returns>
+        public static bool GetConSignedTrades(string sessionKey, out string error, out List<Trade> trades)
+        {
+            bool isSuccess = false;
+            error = string.Empty;
+            trades = null;
+            try
+            {
+                ITopClient client = new DefaultTopClient(Constants.ApiUrl, Constants.AppKey, Constants.AppSecret);
+                TradesSoldGetRequest req = new TradesSoldGetRequest();
+                req.Fields = "tid,buyer_nick,receiver_state,receiver_city,receiver_address,receiver_mobile";
+                req.Status = TradeStatus.SELLER_CONSIGNED_PART;
+                req.StartCreated = DateTime.Now.AddMonths(-3);
+                req.EndCreated = DateTime.Now;
+                TradesSoldGetResponse rsp = client.Execute(req, sessionKey);
+                trades = rsp.Trades;
+                isSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            return isSuccess;
+        }
+
+        /// <summary>
+        /// 获取已发货但买家未确认收货的订单
+        /// </summary>
+        /// <param name="sessionKey"></param>
+        /// <param name="error">如果出错，则储存错误信息，否则为空。</param>
+        /// <param name="trades">如果成功，返回未发货订单的list。否则为null</param>
+        /// <returns>如果操作成功，则返回true。否则为false。</returns>
+        public static bool GetWaitConfirmTrades(string sessionKey, out string error, out List<Trade> trades)
+        {
+            bool isSuccess = false;
+            error = string.Empty;
+            trades = null;
+            try
+            {
+                ITopClient client = new DefaultTopClient(Constants.ApiUrl, Constants.AppKey, Constants.AppSecret);
+                TradesSoldGetRequest req = new TradesSoldGetRequest();
+                req.Fields = "tid,buyer_nick,receiver_state,receiver_city,receiver_address,receiver_mobile";
+                req.Status = TradeStatus.WAIT_BUYER_CONFIRM_GOODS;
+                req.StartCreated = DateTime.Now.AddMonths(-3);
+                req.EndCreated = DateTime.Now;
+                TradesSoldGetResponse rsp = client.Execute(req, sessionKey);
+                trades = rsp.Trades;
+                isSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                error = ex.Message;
+            }
+            return isSuccess;
+        }
+       
     }
 }
---
+++ print/Models/DAL/TradeStatus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace print.Models.DAL
+{
+    public class TradeStatus
+    {
+        /// <summary>
+        /// 等待卖家发货,即:买家已付款
+        /// </summary>
+        public static string WAIT_SELLER_SEND_GOODS { get { return "WAIT_SELLER_SEND_GOODS"; } }
+        /// <summary>
+        /// 卖家部分发货
+        /// </summary>
+        public static string SELLER_CONSIGNED_PART { get { return "SELLER_CONSIGNED_PART"; } }
+        /// <summary>
+        /// 等待买家确认收货,即:卖家已发货
+        /// </summary>
+        public static string WAIT_BUYER_CONFIRM_GOODS { get { return "WAIT_BUYER_CONFIRM_GOODS"; } }
+    }
+}
